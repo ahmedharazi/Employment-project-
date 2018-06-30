c7ec04738addcf681b9d78ac59ff43196b528342
Yeiska <yeiska83@yahoo.com>
2018-06-30 16:18:34 -0400

    getting new employe by js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,82 +1,102 @@
-            // START CODING BELOW!!
+// START CODING BELOW!!
 
-        // Initialize Firebase
-            var config = {
-            apiKey: "AIzaSyCdn5NUnDX9-HQGir0lKw8EFvzsOanEexU",
-            authDomain: "class-project-7a450.firebaseapp.com",
-            databaseURL: "https://class-project-7a450.firebaseio.com",
-            projectId: "class-project-7a450",
-            storageBucket: "",
-            messagingSenderId: "713188156272"
-          };
-          firebase.initializeApp(config);
-         
-             // Create a variable to reference the database
-             var database = firebase.database();
-         
-             // Initial Values
-             var name = "";
-             var role = "";
-             var StartDate= 0;
-             var MonthlyRate = 0;
-            
-         
-             // Capture Button Click
-    $("#add-user").on("click", function(event) {
-                // Don't refresh the page!
-                event.preventDefault();
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCdn5NUnDX9-HQGir0lKw8EFvzsOanEexU",
+    authDomain: "class-project-7a450.firebaseapp.com",
+    databaseURL: "https://class-project-7a450.firebaseio.com",
+    projectId: "class-project-7a450",
+    storageBucket: "",
+    messagingSenderId: "713188156272"
+};
+firebase.initializeApp(config);
 
-            // YOUR TASK!!!
-            // Code in the logic for storing and retrieving the most recent user.
-            // Don't forget to provide initial data to your Firebase database.
-          firebase.initializeApp(config);
-            name = $("#name-term").val().trim();
-            role = $("#role-term").val().trim();
-            StartDate = $("#start-date").val().trim();
-            MonthlyRate = $("#monthly-rate").val().trim();
-      
-      database.ref().push({
-            name: name,
-            role: role,
-            StartDate: StartDate,
-            MonthlyRate: MonthlyRate,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-          });
-    
-        });
-    
-        // Firebase watcher + initial loader HINT: .on("value")
-        
-    database.ref().on("child_added", function(snapshot) {
-        
-                // Log everything that's coming out of snapshot
-            console.log(snapshot.val());
-            console.log(snapshot.val().name);
-            console.log(snapshot.val().role);
-            console.log(snapshot.val().StartDate);
-            console.log(snapshot.val().MonthlyRate);
-      
-            // Change the HTML to reflect
-            $("#name-term").text(snapshot.val().name);
-            $("#role-term").text(snapshot.val().email);
-            $("#start-date").text(snapshot.val().age);
-            $("#monthly-rate").text(snapshot.val().MonthlyRate);
+// Create a variable to reference the database
+var database = firebase.database();
 
-            var newRow = $("<tr>").append(
-                $("<td>").text(empName),
-                $("<td>").text(empRole),
-                $("<td>").text(empStartPretty),
-                $("<td>").text(empMonths),
-                $("<td>").text(empRate),
-                $("<td>").text(empBilled)
-              );
-              // Append the new row to the table
-              $("#employee-table > tbody").append(newRow);
-             });
-      
-            // Handle the errors
-    }, function(errorObject) {
-                console.log("Errors handled: " + errorObject.code);
-            });
+// Capture Button Click
+$("#add-user").on("click", function (event) {
+    // Don't refresh the page!
+    event.preventDefault();
 
-            database.ref().orderByChild("dataAdded").limitToLast
+    // YOUR TASK!!!
+    // Code in the logic for storing and retrieving the most recent user.
+    // Don't forget to provide initial data to your Firebase database.
+
+    var name = $("#name-term").val().trim();
+    var role = $("#role-term").val().trim();
+    var StartDate = $("#start-date").val().trim();
+    var MonthlyRate = $("#monthly-rate").val().trim();
+
+    // Initial Values
+    var newEmp = {
+        name : name,
+        role : role,
+        StartDate : StartDate,
+        MonthlyRate : MonthlyRate
+    };
+
+    database.ref().push(newEmp);
+
+    console.log(newEmp.name);
+    console.log(newEmp.role);
+    console.log(newEmp.start);
+    console.log(newEmp.rate);
+
+    alert("Employee successfully added");
+
+    // Clears all of the text-boxes
+    $("#add-user").val("");
+    $("#role-term").val("");
+    $("#start-date").val("");
+    $("#monthly-rate").val("")
+});
+
+// Firebase watcher + initial loader HINT: .on("value")
+
+database.ref().on("child_added", function (snapshot) {
+
+    // Log everything that's coming out of snapshot
+    console.log(snapshot.val());
+    console.log(snapshot.val().name);
+    console.log(snapshot.val().role);
+    console.log(snapshot.val().StartDate);
+    console.log(snapshot.val().MonthlyRate);
+
+    // Change the HTML to reflect
+    var empName = snapshot.val().name;
+    var empRole = snapshot.val().role;
+    var empStart = snapshot.val().startDate;
+    var empMonthlyRate = snapshot.val().MonthlyRate;
+    // Prettify the employee start
+    //var empStart = moment.unix(empStart).format("MM/DD/YYYY");
+
+    // Calculate the months worked using hardcore math
+    // To calculate the months worked
+    //var empMonths = moment().diff(moment(empStart, "X"), "months");
+    //console.log(empMonths);
+
+    // Calculate the total billed rate
+    //var empBilled = empMonths * empRate;
+    //console.log(empBilled);
+
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(empName),
+        $("<td>").text(empRole),
+        $("<td>").text(empStart),
+        $("<td>").text(empMonthlyRate),
+        //$("<td>").text(empRate),
+        //$("<td>").text(empBilled)
+    );
+    // Append the new row to the table
+    $("#add-employee-row > tbody").append(newRow);
+});
+
+//             // Handle the errors
+//     }, function(errorObject) {
+//     console.log("Errors handled: " + errorObject.code);
+// });
+
+// database.ref().orderByChild("dataAdded");
+//         });
