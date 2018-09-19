20cb338598bddb8a4dae48aa58f05b23223a144c
ahmed Alharazi <ahmed.harazi22@gmail.com>
2018-09-19 18:13:02 -0400

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,48 +21,45 @@ var monthly = "";
 var total = "";
 var worked = "";
 
-// Capture Button Click
-$("#add-user").on("click", function () {
-    event.preventDefault();
+$("#add-employee").on("click", function(){
+      event.preventDefault();
 
-    name = $("#name-term").val().trim();
-    role = $("#role-term").val().trim();
-    date = $("#start-date").val().trim();
-    monthly = $("#monthly-rate").val().trim();
+      name = $("#employee-name").val().trim();
+      role = $("#role").val().trim();
+      date = $("#start-date").val().trim();
+      monthly = $("#monthly-rate").val().trim();
 
-    database.ref().push({
-        name: name,
-        role: role,
-        date: date,
-        monthly: monthly,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-
+      database.ref().push({
+          name: name,
+          role: role,
+          date: date,
+          monthly: monthly,
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+      });
     });
 
-});
+     database.ref().on("child_added", function(childSnapshot){
+         console.log(childSnapshot.val().name);
+         console.log(childSnapshot.val().role);
+         console.log(childSnapshot.val().date);
+         console.log(childSnapshot.val().monthly);
 
-database.ref().on("child_added", function (childSnapshot) {
-    console.log(childSnapshot.val().name);
-    console.log(childSnapshot.val().role);
-    console.log(childSnapshot.val().date);
-    console.log(childSnapshot.val().monthly);
+         var newtr = $("<tr>");
+         var nametd = $("<td>").text(childSnapshot.val().name);
+         var roletd = $("<td>").text(childSnapshot.val().role);
+         var datetd = $("<td>").text(moment(childSnapshot.val().date).format("MMM Do YYYY"));
+         var workedtd = $("<td>").text(moment(childSnapshot.val().date).diff(moment(), "months") * -1);
+         var monthlytd = $("<td>").text(childSnapshot.val().monthly);
 
-    var newtr = $("<tr>");
-    var nametd = $("<td>").text(childSnapshot.val().name);
-    var roletd = $("<td>").text(childSnapshot.val().role);
-    var datetd = $("<td>").text(moment(childSnapshot.val().date).format("MMM Do YYYY"));
-    var workedtd = $("<td>").text(moment(childSnapshot.val().date).diff(moment(), "months") * -1);
-    var monthlytd = $("<td>").text(childSnapshot.val().monthly);
+         newtr.append(nametd);
+         newtr.append(roletd);
+         newtr.append(datetd);
+         newtr.append(workedtd);
+         newtr.append(monthlytd);
 
-    newtr.append(nametd);
-    newtr.append(roletd);
-    newtr.append(datetd);
-    newtr.append(workedtd);
-    newtr.append(monthlytd);
-
-    $(".row tbody").append(newtr);
+         $(".table tbody").append(newtr);
 
 
-}, function (errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-});
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
